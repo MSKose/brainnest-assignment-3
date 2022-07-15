8d429ab99c5b5f4283ab9d5dc0c269df9f8bc90b
MSKose <saitmustafa1998@gmail.com>
2022-07-15 16:40:49 +0200

    final commit

--- app.js
+++ app.js
@@ -1,15 +1,45 @@
-function computerPlay(playerSelection, computerSelection = Math.floor(Math.random() * 3)) {
-    let arr = ['Rock', 'Paper', 'Scissors']
-    let computer = arr[computerSelection]
-    let player = playerSelection.toLowerCase()
-    let playerCap = player.charAt(0).toUpperCase() + player.slice(1);
-    if (player === computer.toLowerCase()) {
-        return "Tie!"
-    } else if ((player === 'rock' && computer === 'Scissors') || (player === 'paper' && computer === 'Rock') || (player === 'scissors' && computer === 'Paper')) {
-        return `You Win! ${playerCap} beats ${computer}`
-    } else  {
-        return `You Lose! ${computer} beats ${playerCap}`
-    }  
-}
+// function computerPlay() {
+//     let arr = ['Rock', 'Paper', 'Scissors']
+//     let computer = arr[Math.floor(Math.random() * 3)]
+//     return computer
+// }
 
-console.log(computerPlay('RocK'))
+// function playRound(a, b) {
+//     let player = a.toLowerCase()
+//     let playerCap = player.charAt(0).toUpperCase() + player.slice(1);
+//     if (player === (b).toLowerCase()) {
+//         return "Tie!"
+//     } else if ((player === 'rock' && (b) === 'Scissors') || (player === 'paper' && (b) === 'Rock') || (player === 'scissors' && (b) === 'Paper')) {
+//         return `You Win! ${playerCap} beats ${(b)}`
+//     } else  {
+//         return `You Lose! ${(b)} beats ${playerCap}`
+//     }  
+// }
+
+// const playerSelection = 'Rock'
+// const computerSelection = computerPlay()
+// console.log(playRound(playerSelection, computerSelection))
+
+function game() {
+    let arr = ['Rock', 'Paper', 'Scissors']
+    let playerCount = 0
+    let computerCount = 0 
+    for (let i=1; i<=5; i++) {
+        const playerSelection = prompt("Type your choice");
+        let player = playerSelection.toLowerCase()          // making sure to have case insensitive inputs
+        let computer = arr[Math.floor(Math.random() * 3)]   // computer chosing randomly from arr
+        if (player === (computer).toLowerCase()) {
+            playerCount++
+            computerCount++
+            console.log('Tie')
+        } else if ((player === 'rock' && (computer) === 'Scissors') || (player === 'paper' && (computer) === 'Rock') || (player === 'scissors' && (computer) === 'Paper')) {
+            playerCount++
+            console.log(`You win, computer's choice was ${computer}`);
+        } else  {
+            computerCount++
+            console.log(`You lose, computer's choice was ${computer}`);
+        } 
+    }
+    return playerCount > computerCount ? `You won` : `You lost`
+}
+console.log(game());
